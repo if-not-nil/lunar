eccf6ae16aab617498116b7ba2a2c67a9eb9441f
if-not-nil <62560250+if-not-nil@users.noreply.github.com>
2025-07-25 00:49:47 +0300

    little bug fixes

--- nt/nt.lua
+++ nt/nt.lua
@@ -5,7 +5,8 @@ local editor = os.getenv("EDITOR") or error("set your $EDITOR environment variab
 local location = os.getenv("NT_DIR") or os.getenv("HOME") .. "/nt-notes"
 
 local find_command = [[cd %s && find -name '*.md' 2>/dev/null]]
-local fzf_command = [[cat %s | fzf --style full --prompt 'nt✴ ' --preview-window=%s:wrap --preview '$(%s preview {})']]
+local fzf_command =
+[[cat %s | fzf --reverse --style full --prompt 'nt✴ ' --preview-window=%s:wrap --preview '$(%s preview {})']]
 local fzf_string = "%s @ %s (%s)\n"
 local edit_cmd = "cd " .. location .. " && $EDITOR " .. location .. "/%s"
 
@@ -14,13 +15,9 @@ local function dir_exists(path)
   return ok == true or ok == 0
 end
 if not dir_exists(location) then
-  ---@diagnostic disable-next-line: cast-local-type
-  if not location then
-    location = os.getenv("HOME") .. "/nt-notes"
-    os.execute("mkdir " .. location)
-    if os.getenv("NT_USE_GIT") ~= "false" then
-      os.execute("cd " .. location .. " && git init .")
-    end
+  os.execute("mkdir -p " .. location)
+  if os.getenv("NT_USE_GIT") ~= "false" then
+    os.execute("cd " .. location .. " && git init .")
   end
 end
 
@@ -61,11 +58,12 @@ local function trim(s)
 end
 
 local function get_title(path)
-  local fd = io.open(path, "r") or error("idk")
+  local fd = io.open(path, "r")
+  if not fd then return path end
   local first_line = fd:lines()()
   if not first_line then return "./" .. path:match("^.+/(.+)$") end
   if string.match(first_line, "^#") then
-    return (trim(first_line:sub(2)))
+    return trim(first_line:match("^#%s*(.*)") or path:match("^.+/(.+)$"))
   end
 end
 
@@ -108,7 +106,7 @@ local function tui()
     local stat = io.popen("stat -c %Y " .. fullpath) or error("huh")
     table.insert(files, {
       path = path,
-      mtime = stat:read(),
+      mtime = tonumber(stat:read()),
       title = get_title(fullpath)
     })
   end
@@ -145,21 +143,26 @@ local function tui()
   end
 end
 
+local function has(cmd)
+  local ok = os.execute("command -v " .. cmd .. " > /dev/null 2>&1")
+  return ok == 0 or ok == true
+end
+
 if arg[1] == "preview" then
   local path = arg[2]
   if path:match(";%S+") then
     local match = path:match(";(%S+)")
     io.stdout:write("echo -ne ;" .. match .. ":\n " .. commands[match].desc)
   else
-    Cat = "Cat"
-    if os.execute("command -v bat > /dev/null") then
-      Cat = "bat --style=plain --color=always --paging=never"
-    elseif os.execute("command -v batcat > /dev/null") then
-      Cat = "batcat --style=plain --color=always --paging=never"
+    local cat = "cat"
+    if has("batcat") then
+      cat = "batcat --style=plain --color=always --paging=never"
+    elseif has("bat") then
+      cat = "bat --style=plain --color=always --paging=never"
     end
 
     ---@diagnostic disable-next-line: param-type-mismatch
-    print(Cat .. " " .. location .. "/" .. path:last_paren())
+    print(cat .. " " .. location .. "/" .. path:last_paren())
   end
   os.exit(0)
 end
@@ -168,10 +171,10 @@ local function check_deps()
   -- table means optional string means necessary
   for _, dep in pairs({ "fzf", editor, "find", { "git", "sync not available" }, { "bat", "pretty printing not available" } }) do
     if type(dep) == "table" then
-      if not os.execute("command -v " .. dep[1] .. " 1>/dev/null") then
+      if not has(dep[1]) then
         print(dep[1] .. " not found: " .. dep[2])
       end
-    elseif not os.execute("command -v " .. dep .. " 1>/dev/null") then
+    elseif not has(dep) then
       print("dependency " .. dep .. " not found. the program won't run without it!")
       os.exit(0)
     end
